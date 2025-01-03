732591dfaefe601683dd48ec97ac112a5472a095
JamesDM62 <lsufan622000@yahoo.com>
2025-01-02 20:08:07 -0600

    render problems7

--- backend/db/migrations/20241211150954-create-user.js
+++ backend/db/migrations/20241211150954-create-user.js
@@ -51,6 +51,7 @@ module.exports = {
 
   async down(queryInterface, Sequelize) {
     options.tableName = "Users";
+    await queryInterface.removeConstraint('my_project_schema.Reviews', 'Reviews_userId_fkey');
     await queryInterface.dropTable('my_project_schema.Reviews');
     return queryInterface.dropTable(options);
   }
