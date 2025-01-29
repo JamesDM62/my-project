e28d3284af78965ebf1c37549f735ab33301cf01
JamesDM62 <lsufan622000@yahoo.com>
2025-01-28 18:08:38 -0600

    done with setup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -15,4 +15,31 @@ router.get("/api/csrf/restore", (req, res) => {
 
 router.use('/api', apiRouter);
 
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+    ); 
+  });
+
+  router.use(express.static(path.resolve("../frontend/dist")));
+
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+    );
+  });
+
+  if (process.env.NODE_ENV !== 'production') {
+    router.get('/api/csrf/restore', (req, res) => {
+      res.cookie('XSRF-TOKEN', req.csrfToken());
+      return res.json();
+    });
+  }
+}
+
 module.exports = router;
