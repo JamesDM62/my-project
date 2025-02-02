153904c0050e87cf650119102420d016adc8ccf4
JamesDM62 <lsufan622000@yahoo.com>
2025-02-02 12:01:34 -0600

    finished nested buttons

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -2,6 +2,9 @@ import { useDispatch } from "react-redux";
 import { useEffect, useState, useRef } from 'react';
 import { FaUserCircle } from 'react-icons/fa';
 import * as sessionActions from '../../store/session';
+import OpenModalButton from "../OpenModalButton/OpenModalButton";
+import LoginFormModal from "../LoginFormModal/LoginFormModal";
+import SignupFormModal from "../SignupFormModal/SignupFormModal";
 
 function ProfileButton({ user }) {
     const dispatch = useDispatch();
@@ -16,10 +19,8 @@ function ProfileButton({ user }) {
     useEffect(() => {
         if (!showMenu) return;
 
-        if (!showMenu) return;
-
         const closeMenu = (e) => {
-          if (ulRef.current && !ulRef.current.contains(e.target)) {
+          if (!ulRef.current.contains(e.target)) {
             setShowMenu(false);
           }
         };
@@ -37,17 +38,36 @@ function ProfileButton({ user }) {
     const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
   
     return (
-      <div>
+      <div className="credentials-list">
         <button onClick={toggleMenu}>
           <FaUserCircle />
         </button>
         <ul className={ulClassName} ref={ulRef}>
-          <li>{user.username}</li>
-          <li>{user.firstName} {user.lastName}</li>
-          <li>{user.email}</li>
-          <li>
-            <button onClick={logout}>Log Out</button>
-          </li>
+          {user ? (
+            <>
+              <li>{user.username}</li>
+              <li>{user.firstName} {user.lastName}</li>
+              <li>{user.email}</li>
+              <li>
+                <button onClick={logout}>Log Out</button>
+              </li>
+            </>
+          ) : (
+            <div className="modal-list">
+              <li>
+                <OpenModalButton
+                  buttonText="Log In"
+                  modalComponent={<LoginFormModal />}
+                />
+              </li>
+              <li>
+                <OpenModalButton
+                  buttonText="Sign Up"
+                  modalComponent={<SignupFormModal />}
+                />
+              </li>
+            </div>
+          )}
         </ul>
       </div>
     );
